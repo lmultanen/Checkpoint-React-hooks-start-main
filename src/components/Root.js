6564ebe29870bf51b1e28e27bba73571993e09cb
Luke Multanen <lmultanen.uchicago@gmail.com>
2022-08-18 17:33:15 -0400

    page is loading pets list using axios

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PetList from './PetList';
 
 // We'll render these sample pets for now. Later, we'll instead fetch the list
 // of pets from the server! We won't need samplePets after that.
-import samplePets from '../petdata';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const Root = () => {
+  // async function getPets() {
+  //   try { 
+  //     const {data} = await axios.get('/api/pets')
+  //     return data;
+  //   } catch(err) {
+  //     console.err("Something went wrong",err);
+  //   }
+  // }
+  // const pets = getPets();
+  // console.log(pets)
+  // let pets = [];
+  const [petList, setPetList] = useState([]);
+
+  useEffect(() => {
+    async function getPets() {
+      try {
+        const {data} = await axios.get('/api/pets');
+        setPetList(data);
+      } catch (err) {
+        console.error("something went wrong fetching pets", err)
+      }
+    }
+    getPets();
+  },[])
 
   return (
     <>
       <h1>Adoption Center</h1>
-      <PetList pets={samplePets} />
+      <PetList pets={petList} />
     </>
   )
 }
